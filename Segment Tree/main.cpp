e53ceb90f648dbff4837e53e755978c6083d8702
Ramandeep Singh Trehan <ramandeeptrehan@gmail.com>
2016-10-14 15:31:06 +0530

    Initial Commit

---
+++ Segment Tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Segment Tree
+//
+//  Created by Ramandeep Singh Trehan on 14/10/16.
+//  Copyright © 2016 Ramandeep Singh Trehan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
